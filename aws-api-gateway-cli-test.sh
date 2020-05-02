c4f751f7854f2407b6b8e904ce09c61f0ac33fd2
Robert Buren <robert@buren.se>
2020-05-02 16:25:41 +0200

    debugging backend

--- aws-api-gateway-cli-test.sh
+++ aws-api-gateway-cli-test.sh
@@ -7,7 +7,7 @@ npx aws-api-gateway-cli-test \
 --app-client-id='14uf685cj51q5dulm1hi9846bh' \
 --cognito-region='eu-west-1' \
 --identity-pool-id='eu-west-1:d9ce43e0-4639-4eff-a025-d6a84cf7e6a2' \
---invoke-url='https://wh86p8i6v6.execute-api.eu-west-1.amazonaws.com/prod' \
+--invoke-url='https://dbnjsqbjji.execute-api.eu-west-1.amazonaws.com/prod' \
 --api-gateway-region='eu-west-1' \
 --path-template='/notes' \
 --method='POST' \
--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -4,6 +4,7 @@ export default function handler(lambda) {
       .then(() => lambda(event, context))
       .then((responseBody) => [200, responseBody])
       .catch((e) => {
+        console.log(e);
         return [500, { error: e.message }];
       })
       .then(([statusCode, body]) => ({
